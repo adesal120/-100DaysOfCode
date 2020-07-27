b2e33a18919d0b04e843aceac40b0a1a512f2f94
Ibukunoluwa salau <salau_ibukunoluwa@yahoo.com>
2020-07-27 21:47:34 +0100

    Add files via upload

---
+++ JavaScript on The Web.js
@@ -0,0 +1,54 @@
+/* Functions */
+
+function multiply (a,b) {
+    if (a>10 || b >10) {
+        return ("It is Hard");
+}
+    else
+    {
+return a*b }
+}
+
+prompt("What is Your Name?")
+
+function Multiply (a,b) {
+    alert(a*b)
+} 
+
+function SayHello () {
+    console.log ("Great")
+}
+
+var SayGo = function () {
+    alert ("God is Good")
+}
+
+/* Arrays */
+
+var list = ["Grace", "Tola", "Titi", "Shade"]
+console.log (list[3]);
+
+var numbers = [1,2,3,4,5,6,7,8,9,10]
+console.log (numbers[4])
+
+var List = ["tiger", "Cat", "Goat", "Pig"]
+ 
+List.push ("Elephant")
+List.pop ()
+List.shift ()
+List.sort ()
+
+
+/* Objects */
+
+var user = {
+    name:"Ibukunoluwa Salau",
+    age:34,
+    College: "Federal University of Agriculture Abeokuta",
+    MaritalStatus: "Single"
+}
+
+
+user.name
+user.age
+
