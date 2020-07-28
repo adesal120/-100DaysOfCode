4b7d36abe5b358b109e8e709a7aa9a9f9b0fd4f0
Ibukunoluwa salau <salau_ibukunoluwa@yahoo.com>
2020-07-28 23:43:21 +0100

    Added JavaScript Conditionals with Examples

--- JavaScript on The Web.js
+++ JavaScript on The Web.js
@@ -1,4 +1,4 @@
-/* Functions */
+/* JavaScript Functions */
 
 function multiply (a,b) {
     if (a>10 || b >10) {
@@ -23,7 +23,7 @@ var SayGo = function () {
     alert ("God is Good")
 }
 
-/* Arrays */
+/* JavaScript Arrays */
 
 var list = ["Grace", "Tola", "Titi", "Shade"]
 console.log (list[3]);
@@ -39,7 +39,7 @@ List.shift ()
 List.sort ()
 
 
-/* Objects */
+/* JavaScript Objects */
 
 var user = {
     name:"Ibukunoluwa Salau",
@@ -65,3 +65,56 @@ var List = [
 
 List[0].password
 List[1].username
+
+
+/* Building Conditional */
+
+/*Create Database*/
+var Database = [
+    {
+        username: "adesal120",
+        password: "secret",
+        email: "ibukunoluwasalau4@gmail.com"
+    },
+    {
+        username: "James2030",
+        password: "secretpassword",
+        email: "james@microsoft.com"
+    },
+    {
+        username: "adesal2030",
+        password: "secretsin",
+        email: "adesal@google.com"
+    }
+]
+
+var Newsfeed = [
+    {
+        username: "adesal120",
+        timeline: "God is the Greatest in the World"
+    },
+    {
+        username: "James2030",
+        timeline: "Jesus is the Greatest in the World"
+    },
+    {
+        username: "adesal2030",
+        timeline: "My Heavely Father is the Greatest in the World"
+    }
+]
+
+/*Create Login Form*/
+var Username = prompt("What is your username")
+var Password = prompt("What is your password?")
+
+/*Build Conditional*/
+
+function SignIn (user,pass) {
+    if (user == Database[0].username && pass == Databse[0].password) {
+        console.log(Newsfeed);
+    } else {
+        alert ("Username or Password is incorrect");
+    }
+}
+
+SignIn (Username,Password)
