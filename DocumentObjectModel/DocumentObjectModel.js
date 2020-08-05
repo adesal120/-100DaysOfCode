e0e4a190f78855ddfe41ed7a6f40948eec462cf7
Ibukunoluwa salau <salau_ibukunoluwa@yahoo.com>
2020-08-05 23:36:21 +0100

    Add files via upload

---
+++ DocumentObjectModel/DocumentObjectModel.js
@@ -0,0 +1,20 @@
+/* DOM Selectors */
+
+document.getElementsbyTagName("h1");
+document.getElementsByClassName("second");
+document.getElementById("first");
+
+document.querySelector("Li");
+document.querySelectorAll("Li");
+
+document.querySelector("Li").getAttribute("random");
+document.querySelector("Li").setAttribute("random","1000");
+
+var ThisSelect = document.querySelector("Li");
+ThisSelect.setAttribute("random", "2500");
+ThisSelect.getAttribute("Li");
+
+h1.classname = "CoolTitle"
+document.querySelector("Li").classList.add("");
+document.querySelector("Li").classList.remove("");
+document.querySelector("Li").classList.toggle("");
